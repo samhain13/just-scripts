3dd4af2902fca4fd3b8a19686545f69f453deafe
Arielle B Cruz <arielle.cruz@gmail.com>
2014-09-14 11:13:26 +0800

    Clean up.

--- random-ipsum.py
+++ random-ipsum.py
@@ -5,7 +5,29 @@
 from random import randint, sample
 from sys import stdout
 
-words = ["a", "ac", "accumsan", "adipiscing", "aenean", "aliquam", "aliquet", "amet", "ante", "arcu", "at", "auctor", "augue", "bibendum", "blandit", "commodo", "condimentum", "congue", "consectetur", "consequat", "convallis", "cras", "cubilia", "curabitur", "cursus", "dapibus", "diam", "dictum", "dictumst", "dignissim", "dolor", "donec", "dui", "duis", "efficitur", "egestas", "eget", "eleifend", "elementum", "elit", "enim", "erat", "eros", "est", "et", "etiam", "eu", "euismod", "ex", "facilisi", "facilisis", "fames", "faucibus", "felis", "fermentum", "feugiat", "finibus", "fringilla", "fusce", "gravida", "habitant", "habitasse", "hac", "hendrerit", "iaculis", "id", "imperdiet", "in", "integer", "interdum", "ipsum", "justo", "lacinia", "lacus", "laoreet", "lectus", "leo", "libero", "ligula", "lobortis", "lorem", "luctus", "maecenas", "magna", "malesuada", "massa", "mattis", "mauris", "maximus", "metus", "mi", "molestie", "mollis", "morbi", "nam", "nec", "neque", "netus", "nibh", "nisi", "nisl", "non", "nulla", "nullam", "nunc", "odio", "orci", "ornare", "pellentesque", "pharetra", "phasellus", "placerat", "platea", "porta", "porttitor", "posuere", "potenti", "praesent", "pretium", "primis", "proin", "pulvinar", "purus", "quam", "quis", "quisque", "rhoncus", "risus", "rutrum", "sagittis", "sapien", "scelerisque", "sed", "sem", "semper", "senectus", "sit", "sodales", "sollicitudin", "suscipit", "suspendisse", "tellus", "tempor", "tempus", "tincidunt", "tortor", "tristique", "turpis", "ullamcorper", "ultrices", "ultricies", "urna", "ut", "varius", "vehicula", "vel", "velit", "venenatis", "vestibulum", "vitae", "vivamus", "viverra", "volutpat", "vulputate"]
+words = ["a", "ac", "accumsan", "adipiscing", "aenean", "aliquam", "aliquet",
+"amet", "ante", "arcu", "at", "auctor", "augue", "bibendum", "blandit",
+"commodo", "condimentum", "congue", "consectetur", "consequat", "convallis",
+"cras", "cubilia", "curabitur", "cursus", "dapibus", "diam", "dictum",
+"dictumst", "dignissim", "dolor", "donec", "dui", "duis", "efficitur",
+"egestas", "eget", "eleifend", "elementum", "elit", "enim", "erat", "eros",
+"est", "et", "etiam", "eu", "euismod", "ex", "facilisi", "facilisis", "fames",
+"faucibus", "felis", "fermentum", "feugiat", "finibus", "fringilla", "fusce",
+"gravida", "habitant", "habitasse", "hac", "hendrerit", "iaculis", "id",
+"imperdiet", "in", "integer", "interdum", "ipsum", "justo", "lacinia", "lacus",
+"laoreet", "lectus", "leo", "libero", "ligula", "lobortis", "lorem", "luctus",
+"maecenas", "magna", "malesuada", "massa", "mattis", "mauris", "maximus",
+"metus", "mi", "molestie", "mollis", "morbi", "nam", "nec", "neque", "netus",
+"nibh", "nisi", "nisl", "non", "nulla", "nullam", "nunc", "odio", "orci",
+"ornare", "pellentesque", "pharetra", "phasellus", "placerat", "platea", "porta",
+"porttitor", "posuere", "potenti", "praesent", "pretium", "primis", "proin",
+"pulvinar", "purus", "quam", "quis", "quisque", "rhoncus", "risus", "rutrum",
+"sagittis", "sapien", "scelerisque", "sed", "sem", "semper", "senectus", "sit",
+"sodales", "sollicitudin", "suscipit", "suspendisse", "tellus", "tempor",
+"tempus", "tincidunt", "tortor", "tristique", "turpis", "ullamcorper",
+"ultrices", "ultricies", "urna", "ut", "varius", "vehicula", "vel", "velit",
+"venenatis", "vestibulum", "vitae", "vivamus", "viverra", "volutpat",
+"vulputate"]
 
 def make_sentence():
     sentence = sample(words, randint(3, 12))
@@ -21,3 +43,4 @@ def make_article():
     return "\n\n".join(paragraphs)
 
 stdout.write("\n" + make_article() + "\n\n")
+
